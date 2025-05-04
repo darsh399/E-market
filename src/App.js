a5b84519d857421209c666d4c6f20dd92c2f2229
darshh399 <sudarshanshinde399@gmail.com>
2025-05-04 22:12:14 +0530

    setup initial UI folder structure for iFruit Market

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Page from './pages/Page';
+function App() {
+
+  return (
+    <div className="App">
+    <h1>Hello</h1>
+    <Page/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DeleteFood.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DeleteFood = () =>{
+    return(
+        <div>
+            <h1>This is AddFood</h1>
+        </div>
+    )
+}
+
+export default DeleteFood;
